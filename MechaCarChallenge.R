5660dd6396640fc709ad97e8247c76631948a1e4
msshahid21 <muhammad.s.shahid@outlook.com>
2022-08-18 00:30:46 -0400

    Answer Questions for deliverable 1

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,18 +2,29 @@
 library(tidyverse)
 
 # Importing the MechaCar_mpg.csv
-mechaCar <- read.csv('MechaCar_mpg.csv')
-
+mechaCar <- read.csv('MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
 
 # Run Multiple Regression Model
 model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechaCar)
 summary(model)
 
-# Importing Suspension
-suspension <- read.csv('Suspension_Coil.csv')
+# Importing Suspension_Coil.csv
+suspension <- read.csv('Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
 
 # Summarize the PSI Column
 total_summary <- suspension %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
 
 # Group by Lots and Summarize
 lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+# T.Test for All Manufacturing Lots
+t.test(suspension$PSI, mu = 1500)
+
+# T.Test for Lot 1
+t.test(subset(suspension, Manufacturing_Lot == "Lot1", select = PSI), mu = 1500)
+
+# T.Test for Lot 2
+t.test(subset(suspension, Manufacturing_Lot == "Lot2", select = PSI), mu = 1500)
+
+# T.Test for Lot 3
+t.test(subset(suspension, Manufacturing_Lot == "Lot3", select = PSI), mu = 1500)
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# MechaCar_Statistical_Analysis
+# MechaCar Statistical Analysis
+
+## Linear Regression to Predict MPG
+
+![Multiple Linear Regression](https://github.com/msshahid21/MechaCar_Statistical_Analysis/blob/main/Images/MLR.png)
+
+1. Which variables/coefficients provided a non-random amount of variance to the mpg values in the dataset?
+- The variables that provided a non-random amount variation were vehicle_length and ground_clearance, as they are shown to have a statistically significant impact on the mpg of the MechaCar.
+
+2. Is the slope of the linear model considered to be zero? Why or why not?
+- As there are two variables shown to have a statistically significant impact on the mpg of the MechaCar, vehicle_length and ground_clearance, the slope of the linear model can be considered to be non-zero.
+
+3. Does this linear model predict mpg of MechaCar prototypes effectively? Why or why not?
+- Yes, the linear model constructed is able to explain 71% of the variation in the mpg (R-Squared Value of 0.7149)
